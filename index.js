219dcffe4d70e1d87f36edcd11b9718312310f8c
zheniatrush <zheniatrush@gmail.com>
2026-05-13 02:51:05 +0300

    autohorize 1.3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+
 const db = require("./db");
 const Users = db.users;
 
@@ -8,37 +9,76 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 
 const express = require("express");
+const session = require("express-session");
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+   session({
+      secret: "my-secret-key",
+      resave: false,
+      saveUninitialized: false,
+   }),
+);
+
 app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/game.html", checkAuthentication, function (req, res) {
+   res.sendFile(path.join(__dirname, "public", "game.html"));
+});
+
+app.use(express.static(path.join(__dirname, "public")));
 //let errorText = document.querySelector(".error_log");
+async function LogHashPassword(LoginPassword) {
+   let LoginHash = await bcrypt.hash(LoginPassword, saltRounds);
+   return LoginHash;
+}
+
 passport.use(
    new LocalStrategy(async function verify(username, password, cb) {
       try {
          const findUsers = await Users.findOne({
             where: {
                login: username,
-               password: password,
             },
          });
 
-         console.log("username:", username);
-         console.log("findUsers:", findUsers);
+         //  console.log("username:", username);
+         // console.log("findUsers:", findUsers);
 
          if (!findUsers) {
             return cb(null, false, {
                message: "Неправильний логін або пароль!",
             });
          }
+         const isPasswordValid = await bcrypt.compare(
+            password,
+            findUsers.password,
+         );
 
+         if (!isPasswordValid) {
+            return cb(null, false, {
+               message: "Неправильний логін або пароль!",
+            });
+         }
          return cb(null, findUsers);
       } catch (err) {
          // errorText.innerHTML = "Невірні дані";
       }
    }),
 );
+
+passport.serializeUser(function (user, done) {
+   done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+   done(null, user);
+});
+
 app.get("/register", function (_, res) {
    res.sendFile(__dirname + "/register.html");
 });
@@ -49,9 +89,7 @@ async function hashPassword(userPassword) {
 }
 app.post("/register", async (req, res) => {
    const hashDone = await hashPassword(req.body.password);
-   console.log(hashDone);
    const { username, email } = req.body;
-   console.log(username);
    try {
       const newUser = await Users.create({
          login: username,
@@ -69,17 +107,26 @@ app.post("/register", async (req, res) => {
 app.post(
    "/login",
    (req, res, next) => {
-      console.log("POST /login спрацював");
-      console.log(req.body);
+      // console.log("POST /login спрацював");
+      // console.log(req.body);
       next();
    },
 
    passport.authenticate("local", {
-      session: false,
-      successRedirect: "/profile",
+      session: true,
+      successRedirect: "/game.html",
       failureRedirect: "/login.html?error=1",
    }),
 );
+
+function checkAuthentication(req, res, next) {
+   if (req.isAuthenticated()) {
+      console.log(req.isAuthenticated());
+      next();
+   } else {
+      res.redirect("/login.html");
+   }
+}
 
 app.listen(3000, () => {
    console.log("Server started on http://localhost:3000");
